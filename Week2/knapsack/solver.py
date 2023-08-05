69c2dae9450f8657c5793ed6d7e976dc88dd0edf
Di Jin <di_jin@brown.edu>
2023-08-04 21:05:39 -0400

    ?

--- Week2/knapsack/solver.py
+++ Week2/knapsack/solver.py
@@ -2,20 +2,33 @@
 # -*- coding: utf-8 -*-
 
 from collections import namedtuple
+from dataclasses import dataclass
+from queue import PriorityQueue as PQueue
 Item = namedtuple("Item", ['index', 'value', 'weight'])
 
-def cal_relaxation(itemlist,weight,value):
-    estimate = value
-    if len(itemlist) > 1:
-        for item in itemlist:
-            if item.weight < weight:
-                estimate += item.value
-            else:
-                estimate += (weight)/item.weight*item.value
-    elif len(itemlist)==1:
-        estimate += itemlist[0].value
-    return estimate
+@dataclass
+class SearchWork:
+    itemlist: list
+    value: int
+    room: int
+    taken: list
 
+    def __lt__(self, other):
+        return self.estimate > other.estimate
+    
+    def do_estimate(self):
+        estimate = float(self.value)
+        remaining_room = self.room
+        for item in self.itemlist:
+            if item.weight < remaining_room:
+                estimate += item.value
+                remaining_room -= item.weight
+            else:
+                estimate += remaining_room/item.weight * item.value
+                remaining_room = 0
+                break
+        self.estimate = estimate
+    
 def cal_branch(tuplelist):
     target = tuplelist[0]
     itemlist = target[0]
@@ -41,6 +54,22 @@ def cal_branch(tuplelist):
     tuplelist.sort(reverse=True,key = lambda choice: choice[3])
     return 0
 
+def children_work(work):
+    item = work.itemlist[0]
+    remaining_room = work.room - item.weight
+    children = []
+    if remaining_room > 0:
+        taken = work.taken[:]
+        taken[item.index] = 1
+        take_work = SearchWork(work.itemlist[1:], work.value + item.value, remaining_room, taken)
+        children.append(take_work)
+    notake_work = SearchWork(work.itemlist[1:], work.value, work.room, work.taken[:])
+    children.append(notake_work)
+    for work in children:
+        work.do_estimate()
+    return children
+
+
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
 
@@ -59,29 +88,34 @@ def solve_it(input_data):
 
     taken = [0]*len(items) #items ->priority queue
     items.sort(reverse=True,key = lambda item: item.value/item.weight)
-    n = 0
-    tuplelist = [(items,0,capacity,cal_relaxation(items,capacity,0),taken)]
-    while n == 0:
-        cal_branch(tuplelist)
-        current_max = tuplelist[0]
-        if (len(current_max[0])==0):
-            result = current_max
-            n = 1
-    # prepare the solution in the specified output format
-    output_data = str(result[1]) + ' ' + str(1) + '\n'
-    output_data += ' '.join(map(str, result[4]))
-    return output_data
+    search_queue = PQueue()
+    initial_work = SearchWork(items[:], 0, capacity, taken[:])
+    initial_work.do_estimate()
+    search_queue.put(initial_work)
+    while True:
+        work = search_queue.get()
+        if len(work.itemlist) == 0:
+            break
+        children = children_work(work)
+        for child in children:
+            search_queue.put(child)
+    print(work)
+    for i, take in enumerate(work.taken):
+        if take:
+            print(i)
+    #initial_work.do_estimate()
+    return 0
 
 if __name__ == '__main__':
     import time
-    start = time.time()
     import sys
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
         with open(file_location, 'r') as input_data_file:
             input_data = input_data_file.read()
+        start = time.time()
         print(solve_it(input_data))
+        ending = time.time()
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
-    ending = time.time()
     print("The time of execution is:",(ending-start)*10**3,"ms")
